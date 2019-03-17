5b642ce6c18470fb1cd0e88f54bea7d401a72231
Krutika Kamdi <kk00128898@techmahindra.com>
2019-03-17 15:34:28 -0700

    no message

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO employee (id, firstname, lastname) VALUES (1, 'Nitesh', 'Kumar')
-INSERT INTO employee (id, firstname, lastname) VALUES (2, 'Rajesh', 'Kumar')
-INSERT INTO employee (id, firstname, lastname) VALUES (3, 'Sumantra', 'Pal')
